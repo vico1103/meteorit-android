f5328c795d5d9087c4d1991090e5599ff8cbddea
vican <xvican02@stud.fit.vubr.cz>
2017-04-24 19:12:45 +0200

    set callback from get meteorit from NASA server

--- Meteorit/app/src/main/java/com/vico/meteorit/module/retrofit/GetMeteorFromApi.java
+++ Meteorit/app/src/main/java/com/vico/meteorit/module/retrofit/GetMeteorFromApi.java
@@ -4,6 +4,9 @@ import android.util.Log;
 
 import com.vico.meteorit.model.MeteoritModel;
 import com.vico.meteorit.module.service.MeteorService;
+import com.vico.meteorit.presenter.MainPresenterInterface;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,23 +16,37 @@ import retrofit2.Response;
  * Created by Peter Vican on 23.4.2017.
  */
 
-public class GetMeteorFromApi implements Callback<MeteoritModel> {
+public class GetMeteorFromApi implements Callback<List<MeteoritModel>> {
 
  public static final String TAG = GetMeteorFromApi.class.getSimpleName();
 
-  public void callGetMeteorFromDate (String timestamp){
+   private MainPresenterInterface mPresenterInterface;
+
+  public GetMeteorFromApi(MainPresenterInterface mainInterface){
+    mPresenterInterface = mainInterface;
+  }
+
+  public void callGetMeteorFromDate (String query){
     RetrofitInstance retrofit = RetrofitInstance.getInstance();
     MeteorService api = retrofit.getMeteorService();
-    api.getMeteorit(timestamp).enqueue(this);
+    Log.d(TAG, "callGetMeteorFromDate(): " + api.getMeteorit(query).request().toString());
+    api.getMeteorit(query).enqueue(this);
   }
 
   @Override
-  public void onResponse(Call<MeteoritModel> call, Response<MeteoritModel> response) {
-    Log.i(TAG, "onResponse():");
+  public void onResponse(Call<List<MeteoritModel>> call, Response<List<MeteoritModel>> response) {
+     if (response.body() != null){
+       for (MeteoritModel test: response.body()
+       ) {
+         Log.d(TAG, "onResponse(): " + test.getName());
+       }
+     }
+
+      mPresenterInterface.loadDataFromNASA(response.body());
   }
 
   @Override
-  public void onFailure(Call<MeteoritModel> call, Throwable t) {
+  public void onFailure(Call<List<MeteoritModel>> call, Throwable t) {
 
   }
 }
