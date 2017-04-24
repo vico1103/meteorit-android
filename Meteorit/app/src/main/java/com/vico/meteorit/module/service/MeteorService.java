296f57ede0687b45752525517e5928f9a4dc2313
vican <xvican02@stud.fit.vubr.cz>
2017-04-24 19:09:06 +0200

    repair meteorit service

--- Meteorit/app/src/main/java/com/vico/meteorit/module/service/MeteorService.java
+++ Meteorit/app/src/main/java/com/vico/meteorit/module/service/MeteorService.java
@@ -2,10 +2,15 @@ package com.vico.meteorit.module.service;
 
 import com.vico.meteorit.model.MeteoritModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Peter Vican on 23.4.2017.
@@ -15,6 +20,6 @@ public interface MeteorService {
 
 
   @Headers("X-App-Token: e0RcXuzbiu5HdihERyrqxJAQH")
-  @GET("/resource/y77d-th95.json?$where=year  >  {yearTimestamp}")
-  Call<MeteoritModel> getMeteorit(@Path("yearTimestamp") String yearTimestamp);
+  @GET("y77d-th95.json")
+  Call<List<MeteoritModel>> getMeteorit(@Query("$where") String queryWithStamp);
 }
